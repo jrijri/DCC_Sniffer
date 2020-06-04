0ef6be1241a837ee153938b15f75c47ff689e944
jrijri <jerome.rioublanc@gmail.com>
2020-06-04 21:59:21 +0200

    origin

---
+++ include/main.h
@@ -0,0 +1,11 @@
+/**
+ * Header for the application.
+ * Used to call all the dependencies
+ * 
+ * Version: 1.0.0 Date: 2020-06-04
+ * 
+ * Author: Jérôme Rioublanc
+ * 
+ */
+
+#include <Arduino.h>
---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+/**
+ *  DCC++ commands sniffer
+ * 
+ * Version: 1.0.0 Date 2020-06-04
+ * 
+ * Author: Jérôme Rioublanc
+ *
+ */
+
+#include "main.h"
+
+void setup()
+ {
+  // put your setup code here, to run once:
+ }
+
+void loop()
+ {
+  // put your main code here, to run repeatedly:
+ }
